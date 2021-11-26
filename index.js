1dc8de37704c61454efc17ec1bda85f919723cf1
Viktoriia Gorianska <viktoriia.gorianska@gmail.com>
2021-11-26 11:50:34 +0100

    fixed Show/Hide button js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 
 
-let bookmarks = document.querySelectorAll('.bookmark i') 
+let bookmarks = document.querySelectorAll('.bookmark i')
 
 bookmarks.forEach((i) => {
-    i.addEventListener('click',() => {
-        console.log('click!', i) 
+    i.addEventListener('click', () => {
+        console.log('click!', i)
 
         i.classList.toggle('fa-regular');
+        i.classList.toggle('fa-solid');
 
     })
 })
 
-
+let btns = document.querySelectorAll('section .btn-light')
+let answers = document.querySelectorAll('section p')
+btns.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        answers[index].classList.toggle('hidden')
+        // if (button.innerText === 'Show Answer') {
+        //     button.innerHTML = '<span>Hide Answer</span>'        
+        // } else {
+        //     button.innerHTML = '<span>Show Answer</span>'
+        // }
+        button.innerText === 'Show Answer' ? 'Hide Answer' : 'Show Answer'
+    })
+})
